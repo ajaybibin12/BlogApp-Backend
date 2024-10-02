535a94bcddaa9ee1b529130a1ba3479a72468514
ajaybibin12 <ajaybibin12@gmail.com>
2024-10-03 03:43:17 +0530

    changed some commands.

--- blog_app/serializers.py
+++ blog_app/serializers.py
@@ -14,14 +14,13 @@ class UserSerializer(serializers.ModelSerializer):
 class UserProfileUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'mobile', 'profile_picture']  # Include 'profile_picture' if you have this field
+        fields = ['username', 'email', 'mobile', 'profile_picture']
 
     def update(self, instance, validated_data):
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
         instance.mobile = validated_data.get('mobile', instance.mobile)
         
-        # If you have a profile picture field, update it
         if 'profile_picture' in validated_data:
             instance.profile_picture = validated_data['profile_picture']
         instance.save()
@@ -38,11 +37,10 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     username_field = 'username'
 
     def validate(self, attrs):
-        # Override the validate method to include custom behavior if needed
         data = super().validate(attrs)
         data.update({
             'username': self.user.username,
-            'user_id': self.user.id  # Add user ID here
+            'user_id': self.user.id
         })
         return data
 
@@ -59,18 +57,18 @@ class BlogPostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BlogPost
-        fields = ['id', 'title', 'content', 'tags', 'author', 'created_at', 'image_base64', 'image']  # Include 'image' in the fields
-        read_only_fields = ['author', 'created_at', 'image']  # Make 'image' read-only
+        fields = ['id', 'title', 'content', 'tags', 'author', 'created_at', 'image_base64', 'image']
+        read_only_fields = ['author', 'created_at', 'image']
 
     def create(self, validated_data):
         tags_data = validated_data.pop('tags')
-        image_base64 = validated_data.pop('image_base64', None)  # Handle the base64 image
+        image_base64 = validated_data.pop('image_base64', None)
 
         # Create the blog post
         post = BlogPost.objects.create(**validated_data)
-
+        
+        # Handle imgages
         if image_base64:
-            # Store the base64 string in the 'image' field (you may want to decode and save as a file instead)
             post.image = image_base64
             post.save()
 
